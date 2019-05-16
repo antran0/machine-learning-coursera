e190b3c7efcdb29b4d6a1e0fbadfae9d660d5bbe
An Tran <an.tran@wsu.edu>
2019-05-16 16:48:16 -0700

    Implemented logic to obtain the optimal values of C and sigma by iterating through all possible pairs of a predefined set of values for [C, sigma].

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,44 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Potential values of C and sigma
+CANDIDATES              = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+% Minimum cross validation prediction error of models trained with some
+% values of C and sigma
+min_prediction_err      = inf;
 
+% Iterate through all candidate values of C
+for C_i = CANDIDATES
+    % Iterate through all candidate values of sigma
+    for sigma_j = CANDIDATES
+        fprintf("Training with parameters [C, sigma] = [%d, %d]\n", ...
+                C_i, sigma_j);
 
+        % Train our model over the training set using the current values of
+        % C and sigma
+        model = svmTrain(X, y, C_i, ...
+                        @(x1,x2) gaussianKernel(x1,x2,sigma_j));
 
+        % Use our trained model to obtain predictions in the cross validation set
+        % and compute the error
+        predictions = svmPredict(model, Xval);
+        error_ij    = mean(double(predictions ~= yval));
 
+        fprintf("Prediction error: %d\n", error_ij);
+
+        % Update the minimum cross validation error and the values of C and sigma
+        % used to obtain that error value
+        if error_ij < min_prediction_err
+            min_prediction_err = error_ij;
+            C = C_i;
+            sigma = sigma_j;
+        endif
+    end
+end
+
+% Optimal values of [C, sigma] for data set 3:
+%   C = 0.1, sigma = 0.1
 
 % =========================================================================
 
